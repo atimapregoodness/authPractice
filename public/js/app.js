8f41f3f39446d1a26aa44754195757f69ec23147
Goodness Atimapre <atimaxgoodness@gmail.com>
2024-04-26 09:08:21 +0100

    just move

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,11 @@
 const closeBtn = document.querySelector('#closeBtn');
 const closeItem = document.querySelector('.errContainer');
+const all = document.querySelector('*');
 
 closeBtn.addEventListener('click', () => {
       closeItem.classList = 'closed';
 });
+
+// all.addEventListener('click', () => {
+//       closeItem.classList = 'closed';
+// });
