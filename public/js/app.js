8ee2fda0191319a44f9ec784c6195e73f9a8831b
Goodness Atimapre <atimaxgoodness@gmail.com>
2024-05-27 20:37:03 +0100

    made changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,17 @@
 const closeBtn = document.querySelector('#closeBtn');
 const closeItem = document.querySelector('.errContainer');
 const all = document.querySelector('*');
+const plswaitBtn = document.querySelector('#plswait');
+
+plswaitBtn.addEventListener('click', () => {
+      plswaitBtn.innerText = 'please wait...';
+});
 
 closeBtn.addEventListener('click', () => {
       closeItem.classList = 'closed';
 });
+
+
 
 // all.addEventListener('click', () => {
 //       closeItem.classList = 'closed';
